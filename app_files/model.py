a3873faec2f472d0470cfa85a62a9f8b29cbcda3
Barbara Bai <barbara.bai@lumiata.com>
2022-02-12 15:06:01 -0500

    Update model.py

--- app_files/model.py
+++ app_files/model.py
@@ -11,7 +11,8 @@ y=dataset.iloc[:,3].values
 
 ###### START CODE HERE##############################
 #define ANY regression Model here
-reg=### Define Model
+from sklearn.tree import DecisionTreeClassifier
+reg=DecisionTreeClassifier()
 reg.fit(X,y)
 print(reg.score)
 #######################################END here
